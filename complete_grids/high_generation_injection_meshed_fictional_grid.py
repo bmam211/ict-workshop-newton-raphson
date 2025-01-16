a76e3472416c9a956dd49904946f0bd8983ee6b8
Nitish Bharambe <nitish.bharambe@alliander.com>
2025-01-16 12:57:17 +0100

    experiment with meshed fiction grid
    Signed-off-by: Nitish Bharambe <nitish.bharambe@alliander.com>

--- complete_grids/high_generation_injection_meshed_fictional_grid.py
+++ complete_grids/high_generation_injection_meshed_fictional_grid.py
@@ -21,7 +21,7 @@ def add_ring_bus_from_to(grid_data, n_ring_num, is_mesh=False):
 
 def create_grid_topo():
     n_rings = 2
-    n_ring_num = 4
+    n_ring_num = 10
     n_meshes = 1
     is_mesh_list = [True] * n_meshes + [False] * (n_rings-n_meshes)
     grid_data = {"id_end": 10, "mv_bus": 0, "line_from": [], "line_to": [], "buses": []}
@@ -33,28 +33,34 @@ def create_grid_topo():
 def create_pp_grid():
     grid_data = create_grid_topo()
     net = pp.create_empty_network()
-    pp.create_bus(net, vn_kv=0.4, index=0)
-    pp.create_bus(net, vn_kv=0.4, index=1)
+    pp.create_bus(net, vn_kv=10.0, index=0)
+    pp.create_bus(net, vn_kv=10.0, index=1)
     pp.create_line_from_parameters(
         net,
         from_bus=0,
         to_bus=1,
         length_km=1.0,
-        r_ohm_per_km=0.01,
-        x_ohm_per_km=0.01,
-        c_nf_per_km=0.01,
+        r_ohm_per_km=1.0,
+        x_ohm_per_km=0.00001,
+        c_nf_per_km=0.0,
         g_us_per_km=0.0,
         max_i_ka=100.0
     )
     pp.create_ext_grid(net, bus=0, vm_pu=1.0, va_degree=0.0)
 
-    pp.create_buses(net, nr_buses=len(grid_data["buses"]), vn_kv=0.4, index=grid_data["buses"])
-    pp.create_lines(net, from_buses=grid_data["line_from"], to_buses=grid_data["line_to"], length_km=[1.0] * len(grid_data["line_from"]), std_type="NAYY 4x150 SE")
-    pp.create_loads(net, buses=grid_data["buses"], p_mw=[0.001] * len(grid_data["buses"]))
+    pp.create_buses(net, nr_buses=len(grid_data["buses"]), vn_kv=10.0, index=grid_data["buses"])
+    pp.create_lines(net, from_buses=grid_data["line_from"], to_buses=grid_data["line_to"], length_km=[10.0] * len(grid_data["line_from"]), std_type="NAYY 4x150 SE")
+    pp.create_sgens(net, buses=grid_data["buses"], p_mw=[1] * len(grid_data["buses"]))
+    # pp.create_sgen(net, bus=1, p_mw=200.0)
     return net
 
 net = create_pp_grid()
-pp.runpp(net)
+# try:
+#     pp.runpp(net, algorithm="nr", init_vm_pu=0.5)  # wrong initial value, parametrize init_vm_pu
+# except LoadflowNotConverged as e:
+#     print("Wrong initial value")
+#     print(e)
+
+pp.runpp(net, algorithm="nr", init="auto")  # correct initial value, parametrize init_vm_pu
+print("Correct initial value")
 print(net.res_bus)
-
-
